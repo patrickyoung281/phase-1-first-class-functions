e04d9c8551f68d5a972c8add9909bf572ef69c0d
Patrick Young <patrickyoung281@gmail.com>
2022-05-12 16:55:08 -0400

    commit tree cb4bbf8b0ca8a78df687b8c0a247c26c7a0fb13c

---
+++ index.js
@@ -0,0 +1,114 @@
+
+
+function receivesAFunction (callBack) {
+    return callBack();
+};
+
+function returnsANamedFunction () {
+    return function numberOne () {};
+};
+
+
+function returnsAnAnonymousFunction () {
+    return function () {};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function receivesAFunction (sampleFunction) {
+    sampleFunction();
+};
+
+function returnsANamedFunction () {
+    return function alpha () {};
+};
+
+function returnsAnAnonymousFunction () {
+    return function () {};
+};
+
+*/
+
+
+/*
+
+function receivesAFunction (sample) {
+    return sample()};
+
+function returnsANamedFunction () {
+    return function alpha() {};
+
+};
+
+function returnsAnAnonymousFunction () {
+    return function () {
+        console.log(greeting);
+    }
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+function receivesAFunction(callback) {
+    return callback()
+};
+
+function returnsANamedFunction () {
+    return function receivesAFunction(name) {
+        console.log(name);
+    }
+}
+
+function returnsAnAnonymousFunction () {
+    return function () {}
+};
+
+*/
